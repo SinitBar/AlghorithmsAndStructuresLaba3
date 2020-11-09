c3803806a88d2dfbf36462d2bcbc4e230ba398bb
SinitBar <varka.sinitsckaya@yandex.ru>
2020-11-09 22:25:21 +0300

    Update QueueAndStack.h

--- QueueAndStack.h
+++ QueueAndStack.h
@@ -4,51 +4,73 @@
 
 using namespace std;
 
-class element
+class Node
 {
-	element* next;
-	int data;
-	void set_next(element*);
-	void set_data(int);
+private:
+	int key;
+	Node* left_child;
+	Node* right_child;
+	Node* set_left_child(int);
+	Node* set_right_child(int);
+	void set_key(int);
+public:
+	Node();
+	Node(int);
+	friend class BinarySearchTree;
+	friend class Element;
+	friend class Queue;
+	friend class Stack;
+	Node* get_left_child();
+	Node* get_right_child();
+	int get_key();
+	~Node();
+};
+
+class Element
+{
+	Element* next;
+	Node* data;
+	void set_next(Element*);
+	void set_data(Node*);
 
 public:
-	element();
+	Element();
+	Element* get_next();
+	Node* get_data();
+	~Element();
 	friend class Queue;
-	friend class
-	element* get_next();
-	int get_data();
-	~element();
+	friend class Stack;
 };
 
 class Queue
 {
 private:
-	element* head; // the first in queue, leaves first
-	element* tail; // the last, new in queue
-	void set_head(element*);
-	void set_tail(element*);
+	Element* head; // the first in queue, leaves first
+	Element* tail; // the last, new in queue
+	void set_head(Element*);
+	void set_tail(Element*);
 public:
 	Queue();
 	int size(); // return a number of elements in queue
 	bool is_empty(); // returns true if queue is empty
-	element* top(); // to know who will leave first
-	element* pop(); // to delete first in queue and return it to user
-	element* push(int); // to add in the end of the queue the element with key-data and return it to user
-	element* back(); // to know who is the last in queue and return it to user
+	Element* top(); // to know who will leave first
+	Element* pop(); // to delete first in queue and return it to user
+	Element* push(Node*); // to add in the end of the queue the element with key-data and return it to user
+	Element* back(); // to know who is the last in queue and return it to user
 	~Queue();
 };
 
 class Stack
 {
 private:
-	element* top;
-	void set_top(element*);
+	Element* top;
+	void set_top(Element*);
 public:
 	Stack();
 	bool is_empty();
 	int size();
-	element* peek(); // to show who's on the top
-	element* pop(); // to delte top and show it
-	element* push(int); // to push on top and show it
+	Element* peek(); // to show who's on the top
+	Element* pop(); // to delte top and show it
+	Element* push(Node*); // to push on top and show it
 	~Stack();
 };
