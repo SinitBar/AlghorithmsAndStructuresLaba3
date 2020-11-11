b145eff23e80f63e8ef8465c5f356fde806bea39
SinitBar <varka.sinitsckaya@yandex.ru>
2020-11-11 05:33:06 +0300

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -11,7 +11,6 @@ public:
 	Queue();
 	int size(); // return a number of elements in queue
 	bool is_empty(); // returns true if queue is empty
-	Element* top(); // to know who will leave first
 	Element* pop(); // to delete first in queue and return it to user
 	Element* push(Node*); // to add in the end of the queue the element with key-data and return it to user
 	//Element* back(); // to know who is the last in queue and return it to user
